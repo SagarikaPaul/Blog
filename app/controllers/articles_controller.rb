3977d50b8afadd15e9e6e0514f836bce0a4c6289
SagarikaPaul <sagarikapaul6@gmail.com>
2020-11-18 17:23:10 +0530

    UPDATE functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
       @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -21,6 +25,16 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
 
   private
   def article_params
